e77b62c2e375805041bab79129392d8f74ecd10a
FS-Adrian <77692518+FS-Adrian@users.noreply.github.com>
2021-04-21 13:58:35 -0500

    Add files via upload

---
+++ Proyecto Modulo 6/Default.js
@@ -0,0 +1,8 @@
+document.getElementById('btn1').addEventListener("click", function(){alert('Pagina en Construccion');});
+document.getElementById('btn2').addEventListener("click", function(){alert('Pagina en Construccion');});
+document.getElementById('btn3').addEventListener("click", function(){alert('Pagina en Construccion');});
+document.getElementById('foro1').addEventListener("click", function(){alert('Pagina en Construccion');});
+document.getElementById('foro2').addEventListener("click", function(){alert('Pagina en Construccion');});
+document.getElementById('sub').addEventListener("click", function(){alert('Informacion enviada exitosamente');});
+document.getElementById('sub2').addEventListener("click", function(){alert('Pagina en Construccion');});
+
---
+++ Proyecto Modulo 6/registro.js
@@ -0,0 +1,5 @@
+document.getElementById('sub').addEventListener("click", displayDate);
+
+function displayDate(){
+	document.getElementById('demo').innerHTML = Date();
+}
